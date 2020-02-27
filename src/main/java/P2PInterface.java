a8cd6fcdad6faa637f949503388703d8f9944498
Aayush Gupta <aayushgupta@1x-nat-vl931-172-30-160-41.wireless.umass.edu>
2020-02-26 19:11:22 -0500

    Added boilerplate code for the interface

---
+++ src/main/java/P2PInterface.java
@@ -0,0 +1,21 @@
+public interface P2PInterface {
+
+    /** This method would search the network for the buyers
+     * @param productName - Contains the product name that the buyer needs to buy
+     * @param hopCount - Decrements after every call
+     */
+    public void lookup(String productName, int hopCount);
+
+    /**
+     * This method will be used by the sellers to respond to a buy request
+     * @param sellerID - Contains the ID of the seller
+     */
+    public void reply(String sellerID);
+
+    /**
+     * This method will be used by the buyers to directly connect to a seller and buy a product
+     * @param sellerID - Contains the ID of the seller
+     */
+    public void buy(String sellerID);
+
+}
---
+++ src/main/java/PeerNode.java
@@ -0,0 +1,6 @@
+public class PeerNode
+{
+    public static void main(String[] args) {
+
+    }
+}
